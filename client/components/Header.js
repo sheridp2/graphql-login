93826c27fb3e90fa178fb9da47fdf4b569f29f71
Patrick Sheriddan <sheridp2@gmail.com>
2020-12-09 17:27:49 -0800

    header added and content changes based on users signin status

---
+++ client/components/Header.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import query from '../queries/CurrentUser'
+
+class Header extends Component {
+  renderButtons(){
+    const { loading, user } = this.props.data
+
+    if(loading){
+      return <div>Loading</div>
+    }
+
+    if(user){
+      return <div>Logout</div>
+    } else {
+      return(
+        <div>Not signed in</div>
+      )
+    }
+
+  }
+
+  render() {
+    return(
+      <nav>
+        <div className="nav-wrapper">
+          {this.renderButtons()}
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default graphql(query)(Header)
